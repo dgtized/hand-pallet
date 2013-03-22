7f81e3577f451218bf9f246730436b038c8f4eec
Charles Comstock <dgtized@gmail.com>
2013-03-21 22:37:45 -0500

    run lein pallet project-init

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/dgtized/hand-pallet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [com.palletops/pallet "0.8.0-beta.5"]
+                 [org.cloudhoist/pallet-vmfest "0.3.0-alpha.2"]
+                 [org.clojars.tbatchelli/vboxjws "4.2.6"]]
+  :repositories {"sonatype"
+                 "http://oss.sonatype.org/content/repositories/releases"}
+  :plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]])
