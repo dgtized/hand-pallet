f7eae38fe19a44f882920d7660b3bd25fd1f72c2
Charles Comstock <dgtized@gmail.com>
2013-03-21 22:30:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# hand-pallet
+
+A pallet tutorial using virtualbox as a provider.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 Charles L.G. Comstock
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject hand-pallet "0.1.0-SNAPSHOT"
+  :description "A pallet tutorial using virtualbox as a provider."
+  :url "https://github.com/dgtized/hand-pallet"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]])
