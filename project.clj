ab86071bd0b97373bc69b8d9c9302d574d134f23
Charles Comstock <dgtized@gmail.com>
2013-03-29 17:27:07 -0500

    upgrade to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/dgtized/hand-pallet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.palletops/pallet "0.8.0-beta.5"]
                  [org.cloudhoist/pallet-vmfest "0.3.0-alpha.2"]
                  [org.clojars.tbatchelli/vboxjws "4.2.6"]]
